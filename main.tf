a7fc0827bb2f87b05aae4aabae0ea220b02892e6
Ashokangithub <158256893+Ashokangithub@users.noreply.github.com>
2024-12-17 21:04:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-    region = "us-west-2a"
-    AWS_ACCESS_KEY_ID=""
-    AWS_SECRET_ACCESS_KEY=""
-}
-
 resource "aws_instance" "foo" {
   ami           = "ami-061dd8b45bc7deb3d" # us-west-2
   instance_type = "t2.micro"
